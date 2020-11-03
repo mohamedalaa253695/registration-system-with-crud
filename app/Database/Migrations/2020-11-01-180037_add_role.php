becb91227c65ba6577c36c45f1f57dc5902179ec
mohamedalaa253695 <mohamedalaa253695@gihub.com>
2020-11-03 22:13:20 +0200

    added  basic database schema

---
+++ app/Database/Migrations/2020-11-01-180037_add_role.php
@@ -0,0 +1,23 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddRole extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+                    'id'                    => ['type' => 'INT'    , 'constraint' => 5   , 'unsigned' => true,'auto_increment' => true ],
+                    'name'                  => ['type' => 'VARCHAR', 'constraint' => 191 , 'null'			 => false],
+                        
+                ]);
+                $this->forge->addKey('id', true);
+                $this->forge->createTable('roles');
+	}
+
+	
+	public function down()
+	{
+		$this->forge->dropTable('roles');
+	}
+}
